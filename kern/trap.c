34605a0bb8184602c038d2f7ef5ba44fc5a4dd14
springfielding <springfielding@hotmail.com>
2020-02-11 00:10:26 +0800

    add some code to ex9

--- kern/trap.c
+++ kern/trap.c
@@ -353,7 +353,27 @@ page_fault_handler(struct Trapframe *tf)
 	//   (the 'tf' variable points at 'curenv->env_tf').
 
 	// LAB 4: Your code here.
+	if (curenv->env_pgfault_upcall) {
+		struct UTrapframe *uf = NULL;
+		if(UXSTACKTOP-PGSIZE <= tf->tf_esp && tf->tf_esp < UXSTACKTOP) {
+			// recursive case
+			uf = (struct UTrapframe *)(tf->tf_esp - sizeof(struct UTrapframe) - 4);
+		} else {
+			// non-recursive case
+			uf = (struct UTrapframe *)(UXSTACKTOP - sizeof(struct UTrapframe));
+		}
 
+		uf->utf_fault_va = fault_va;
+		uf->utf_err = tf->tf_err;
+		uf->utf_eip = tf->tf_eip;
+		uf->utf_eflags = tf->tf_eflags;
+		uf->utf_esp = tf->tf_esp;
+
+		curenv->env_tf.tf_eip = (uintptr_t)curenv->env_pgfault_upcall;
+		curenv->env_tf.tf_esp = (uintptr_t)uf;
+		env_run(curenv);
+	}
+	
 	// Destroy the environment that caused the fault.
 	cprintf("[%08x] user fault va %08x ip %08x\n",
 		curenv->env_id, fault_va, tf->tf_eip);
