97569c71f88cd0b5325599d90f431fe31e9391a2
springfielding <springfielding@hotmail.com>
2020-02-17 21:50:18 +0800

    add some code to ex5

--- kern/e1000.c
+++ kern/e1000.c
@@ -3,7 +3,15 @@
 // LAB 6: Your driver code here
 #include <kern/pmap.h>
 
-#define E1000_STATUS 0x00008
+
+volatile void *bar_va;
+#define E1000REG(offset) (void *)(bar_va + offset)
+
+struct e1000_tdh *tdh;
+struct e1000_tdt *tdt;
+struct e1000_tx_desc tx_desc_array[TXDESCS];
+char tx_buffer_array[TXDESCS][TX_PKT_SIZE];
+
 
 int
 e1000_attachfn(struct pci_func *f)
@@ -11,7 +19,65 @@ e1000_attachfn(struct pci_func *f)
     pci_func_enable(f);
     void *va = mmio_map_region(f->reg_base[0], f->reg_size[0]);
 
-    uint32_t * reg_addr = (uint32_t *)(va + E1000_STATUS);
-    cprintf("mmoi map pa=%08x to va=%08x, status addr=%08x, value=%08x\n", f->reg_base[0], va, reg_addr, *reg_addr);
+    // uint32_t *status_reg = (uint32_t *)E1000REG(E1000_STATUS);
+    // cprintf("mmoi map pa=%08x to va=%08x, status addr=%08x, value=%08x\n", f->reg_base[0], bar_va, status_reg, *status_reg);
+    // assert(*status_reg == 0x80080783);
+    // e1000_transmit_init();
     return 0;
+}
+
+
+static void
+e1000_transmit_init()
+{
+    // Allocate a region of memory for the transmit descriptor list. Software should insure this memory is
+    // aligned on a paragraph (16-byte) boundary. Program the Transmit Descriptor Base Address
+    // (TDBAL/TDBAH) register(s) with the address of the region. TDBAL is used for 32-bit addresses
+    // and both TDBAL and TDBAH are used for 64-bit addresses
+    int i;
+	for (i = 0; i < TXDESCS; i++) {
+		tx_desc_array[i].addr = PADDR(tx_buffer_array[i]);
+		tx_desc_array[i].cmd = 0;
+		tx_desc_array[i].status |= E1000_TXD_STAT_DD;
+	}
+
+    uint32_t *tdbal = (uint32_t *)E1000REG(E1000_TDBAL);
+    *tdbal = PADDR(tx_desc_array);
+
+    uint32_t *tdbah = (uint32_t *)E1000REG(E1000_TDBAH);
+    *tdbah = 0;
+
+    // Set the Transmit Descriptor Length (TDLEN) register to the size (in bytes) of the descriptor ring.
+    // This register must be 128-byte aligned.
+    struct e1000_tdlen *tdlen = (struct e1000_tdlen *)E1000REG(E1000_TDLEN);
+    tdlen->len = TXDESCS;
+
+    // The Transmit Descriptor Head and Tail (TDH/TDT) registers are initialized (by hardware) to 0b
+    // after a power-on or a software initiated Ethernet controller reset. Software should write 0b to both
+    // these registers to ensure this.
+    struct e1000_tdh *tdh = (struct e1000_tdh *)E1000REG(E1000_TDH);
+    tdh->tdh = 0;
+    struct e1000_tdt *tdt = (struct e1000_tdt *)E1000REG(E1000_TDT);
+    tdt->tdt = 0;
+
+    // Initialize the Transmit Control Register (TCTL) for desired operation to include the following:
+    // • Set the Enable (TCTL.EN) bit to 1b for normal operation.
+    // • Set the Pad Short Packets (TCTL.PSP) bit to 1b.
+    // • Configure the Collision Threshold (TCTL.CT) to the desired value. Ethernet standard is 10h.
+    //       This setting only has meaning in half duplex mode.
+    // • Configure the Collision Distance (TCTL.COLD) to its expected value. For full duplex
+    //      operation, this value should be set to 40h. For gigabit half duplex, this value should be set to
+    //      200h. For 10/100 half duplex, this value should be set to 40h.
+    struct e1000_tctl *tctl = (struct e1000_tctl *)E1000REG(E1000_TCTL);
+    tctl->en = 1;
+    tctl->psp = 1;
+    tctl->ct = 0x10;
+    tctl->cold = 0x40;
+
+    // Program the Transmit IPG (TIPG) register with the following decimal values to get the minimum
+    // legal Inter Packet Gap: ipgt=10，ipgr1=4(ipgr2 * 2/3)，ipgr2=6
+    struct e1000_tipg *tipg = (struct e1000_tipg *)E1000REG(E1000_TIPG);
+    tipg->ipgt = 10;
+    tipg->ipgr1 = 4;
+    tipg->ipgr2 = 6;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,6 +6,76 @@
 #define E1000_VENDER_ID_82540EM 0x8086
 #define E1000_DEV_ID_82540EM 0x100E
 
+#define TXDESCS 32
+#define TX_PKT_SIZE 1518
+
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TXD_STAT_DD    0x00000001 /* Descriptor Done */
+#define E1000_TXD_CMD_EOP    0x00000001 /* End of Packet */
+#define E1000_TXD_CMD_RS     0x00000008 /* Report Status */
+
+/*transmit descriptor related*/
+struct e1000_tx_desc
+{
+	uint64_t addr;      // Buffer Address
+	uint16_t length;    // Length is per segment. 
+	uint8_t cso;        //  Checksum Offset
+	uint8_t cmd;        // Command field EOP IFCS IC RS RPS DEXT VLE IDE
+	uint8_t status;     // Status field dd ec lc tu
+	uint8_t css;        // Checksum Start Field
+	uint16_t special;
+}__attribute__((packed));
+
+// Transmit Control register 
+struct e1000_tctl {
+	uint32_t rsv1:   1;
+	uint32_t en:     1; // Transmit Enable
+	uint32_t rsv2:   1;
+	uint32_t psp:    1; // Pad Short Packets 0b = Do not pad. 1b = Pad short packets.
+	uint32_t ct:     8; // Collision Threshold
+	uint32_t cold:   10;// Collision Distance
+	uint32_t swxoff: 1; // Software XOFF Transmission
+	uint32_t rsv3:   1;
+	uint32_t rtlc:   1; // Re-transmit on Late Collision
+	uint32_t nrtu:   1; // No Re-transmit on underrun (82544GC/EI only)
+	uint32_t rsv4:   6;
+};
+
+// Transmit InterPacket Gap 
+struct e1000_tipg {
+	uint32_t ipgt:   10;    // IPG Transmit Time
+	uint32_t ipgr1:  10;    // IPG Receive Time 1
+	uint32_t ipgr2:  10;    // IPG Receive Time 2
+	uint32_t rsv:    2;
+};
+
+// Transmit Descriptor Length register (TDLEN)
+struct e1000_tdlen {
+	uint32_t zero: 7;   // Ignore on write. Reads back as 0b
+	uint32_t len:  13;  // Descriptor Length. Number of bytes allocated to the transmit descriptor circular buffer.
+	uint32_t rsv:  12;
+};
+
+// Transmit Descriptor Tail register 
+struct e1000_tdt {
+	uint16_t tdt;   // Transmit Descriptor Tail
+	uint16_t rsv;
+};
+
+// Transmit Descriptor Head register (TDH)
+struct e1000_tdh {
+	uint16_t tdh;   // Transmit Descriptor Head
+	uint16_t rsv;
+};
+
 int e1000_attachfn(struct pci_func *pcif);
+static void e1000_transmit_init();
 
 #endif	// JOS_KERN_E1000_H
