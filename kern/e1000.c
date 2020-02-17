6a0d305d5421204d118da96cae5f3757dfe8c424
springfielding <springfielding@hotmail.com>
2020-02-17 17:52:51 +0800

    finish ex4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,10 +1,17 @@
 #include <kern/e1000.h>
 
 // LAB 6: Your driver code here
+#include <kern/pmap.h>
+
+#define E1000_STATUS 0x00008
 
 int
 e1000_attachfn(struct pci_func *f)
 {
     pci_func_enable(f);
+    void *va = mmio_map_region(f->reg_base[0], f->reg_size[0]);
+
+    uint32_t * reg_addr = (uint32_t *)(va + E1000_STATUS);
+    cprintf("mmoi map pa=%08x to va=%08x, status addr=%08x, value=%08x\n", f->reg_base[0], va, reg_addr, *reg_addr);
     return 0;
 }
