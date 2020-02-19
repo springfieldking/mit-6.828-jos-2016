862e0732f10b9aaef54bf7ece0bc103afcdfe08d
springfieldking <springfieldking@hotmail.com>
2020-02-19 22:31:16 +0800

    add some code to ex12

--- net/input.c
+++ net/input.c
@@ -2,6 +2,8 @@
 
 extern union Nsipc nsipcbuf;
 
+#define RX_PKT_SIZE 1518
+
 void
 input(envid_t ns_envid)
 {
@@ -13,4 +15,17 @@ input(envid_t ns_envid)
 	// Hint: When you IPC a page to the network server, it will be
 	// reading from it for a while, so don't immediately receive
 	// another packet in to the same physical page.
+
+	while(1) {
+		int recv_len;
+		char buf[RX_PKT_SIZE];
+		if ((recv_len = sys_pkt_recv(buf, RX_PKT_SIZE)) < 0) {
+			continue;
+		}
+		memcpy(nsipcbuf.pkt.jp_data, buf, recv_len);
+		nsipcbuf.pkt.jp_len = recv_len;
+		ipc_send(ns_envid, NSREQ_INPUT, &nsipcbuf, PTE_P|PTE_U|PTE_W);
+
+		sys_yield();
+	}
 }
